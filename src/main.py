285f90c82fdb73d5ccd26495a1bf821381fa8e13
Angela Santin Ceballos <angela.d.santin@gmail.com>
2024-03-10 13:10:36 +0100

    Add linting

--- src/main.py
+++ src/main.py
@@ -1,5 +1,4 @@
 import numpy as np
-import matplotlib.pyplot as plt
 
 from visualise import draw
 
@@ -13,30 +12,27 @@ class Value:
         self.label = label
         self._backward = lambda: None
 
-    
     def __repr__(self) -> str:
         return f'Value({self.data}) {self._op}'
 
-    # def __radd__(self, other):
-    #     return Value(self.data + other, children=(self, other), op='+')
-
     def __add__(self, other) -> "Value":
         out = Value(self.data + other.data, children=(self, other), op='+')
+
         def _backward():
             self.grad += 1*out.grad
             other.grad += 1*out.grad
         self._backward = _backward
         return out
 
-    
     def __mul__(self, other) -> "Value":
         out = Value(self.data * other.data, children=(self, other), op='*')
+
         def _backward():
             self.grad += other.data*out.grad
             other.grad += self.data*out.grad
         self._backward = _backward
         return out
-    
+
     def tanh(self):
         def _backward():
             self.grad += (1 - np.tanh(self.data)**2)*out.grad
@@ -45,13 +41,15 @@ class Value:
         return out
 
 
-
 a = Value(2, label='a')
 b = Value(3, label='b')
 c = Value(5, label='c')
-d = a*b; d.label = 'd'
-e = d+c; e.label = 'e'
-x = e.tanh(); x.label = 'x' 
+d = a*b
+d.label = 'd'
+e = d+c
+e.label = 'e'
+x = e.tanh()
+x.label = 'x'
 
 
 dot = draw(x)
